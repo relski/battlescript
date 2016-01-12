ddfad6ab3355b39b13a088797119f7c29bb03510
Jorel Cariño <jorelcarino@yahoo.com>
2016-01-12 17:32:29 +0800

    Update hero_constructor.js
    Fixed reference error

--- hero_constructor.js
+++ hero_constructor.js
@@ -3,9 +3,9 @@ var Unit = function (name, healthPoints, attackPower, unitType) {
     this.healthPoints = healthPoints;
     this.attackPower = attackPower;
     this.unitType = unitType;
-}
+};
 
-var hero1 = new Unit (Gwyn, 60, 3, hero)
-var enemy1 = new Unit (Ogre, 70, 4, monster)
+var hero1 = new Unit ('Gwyn', 60, 3, 'hero');
+var enemy1 = new Unit ('Ogre', 70, 4, 'monster');
 
-alert(hero1.unitType)
+alert(hero1.healthPoints);
